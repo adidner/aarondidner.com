455752db583c3ddf744f4ea24967b9c0ab4c5333
Aaron Didner <adidner@gmail.com>
2023-03-02 19:08:39 -0500

    added chart to main nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { graphql, Link, useStaticQuery } from 'gatsby'
+import React from "react"
+import { graphql, Link, useStaticQuery } from "gatsby"
 
 export default function Navbar() {
   const data = useStaticQuery(graphql`
@@ -14,16 +14,26 @@ export default function Navbar() {
   const { title } = data.site.siteMetadata
 
   const activeStyle = {
-    borderColor: 'white',
+    borderColor: "white",
   }
 
   return (
     <nav>
-      <h1><Link to="/" activeStyle={activeStyle}>{ title }</Link></h1>
+      <h1>
+        <Link to="/" activeStyle={activeStyle}>
+          {title}
+        </Link>
+      </h1>
       <div className="links">
-        
-        <Link to="/resume" activeStyle={activeStyle}>Resume</Link>
-        <Link to="/reading" activeStyle={activeStyle}>Reading</Link>
+        <Link to="/resume" activeStyle={activeStyle}>
+          Resume
+        </Link>
+        <Link to="/reading" activeStyle={activeStyle}>
+          Reading
+        </Link>
+        <Link to="/flowchart" activeStyle={activeStyle}>
+          Chart
+        </Link>
         {/* <Link to="/personal-projects" activeStyle={activeStyle}>Personal Projects</Link>
         <Link to="/projects" activeStyle={activeStyle}>Projects</Link> */}
       </div>
